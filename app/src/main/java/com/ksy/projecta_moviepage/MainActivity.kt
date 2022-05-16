1002726df0bdcdd848f3459a26ef7cd27d7eb1d9
ShinYoung-Kim <lukey0515@naver.com>
2022-05-17 01:46:03 +0900

    프로젝트A.영화상세 화면 만들기

---
+++ app/src/main/java/com/ksy/projecta_moviepage/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ksy.projecta_moviepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import com.ksy.projecta_moviepage.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
